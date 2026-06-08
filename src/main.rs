63ad743eb60c927fed9a14493806b921ce4bec74
Tim Dacalos <2004tim2004@gmail.com>
2026-06-07 19:03:24 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
 
+    let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
-    
+    println!("{}: command not found", command.trim());
 
 }
